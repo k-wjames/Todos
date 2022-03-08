652dcd653efa478a45fddb7c56df131811391516
w.kariukijames <kwjames9326@gmail.com>
2022-03-08 18:50:35 +0300

    Room DB created

--- app/src/main/java/ke/co/ideagalore/todos/Todo.kt
+++ app/src/main/java/ke/co/ideagalore/todos/Todo.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "todosTable")
+@Entity(tableName = "todoTable")
 class Todo(
     @ColumnInfo(name = "title") val noteTitle: String,
     @ColumnInfo(name = "description") val noteDescription: String,
